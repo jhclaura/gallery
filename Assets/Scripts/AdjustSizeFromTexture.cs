1ad9df30548403d8dfa1e0135c92bace5f099fb9
jhclaura <jhclaura@gmail.com>
2016-08-17 12:54:05 -0400

    init project

---
+++ Assets/Scripts/AdjustSizeFromTexture.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class AdjustSizeFromTexture : MonoBehaviour {
+
+	Material mat;
+	Texture tex;
+
+
+	void Start () {
+		mat = GetComponent<Renderer> ().material;
+
+//		Debug.Log ( Pathing.AppContentDataUri );
+
+		AdjustSize ();
+
+//		string myStreamingAssetsPath = Pathing.AppContentDataUri+"";
+	}
+
+	public void AdjustSize() {
+		tex = mat.GetTexture ("_MainTex");
+//		Debug.Log ("art: " + tex.name);
+		string relativePath = tex.name + ".jpg";
+		string fullPath = Path.Combine( Application.streamingAssetsPath, relativePath );
+
+		// Vector2Int imgSize  = ImageHeader.GetDimensions(Application.persistentDataPath+"/Images/Amedeo_Modigliani_Christina.jpg");
+//		Vector2Int imgSize  = ImageHeader.GetDimensions(Application.dataPath+"/Images/" + tex.name + ".jpg");
+		Vector2Int imgSize  = ImageHeader.GetDimensions(fullPath);
+		Debug.Log("art: " + tex.name + ", size.x =" + imgSize.x + ", size.y =" + imgSize.y);
+
+		int imgX = imgSize.x;
+		int imgY = imgSize.y;
+		float scaleNumber = (float)imgY / imgX;
+		Debug.Log("scaleNumber: " + scaleNumber);
+
+		gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x,
+			gameObject.transform.localScale.y,
+			gameObject.transform.localScale.x * scaleNumber);
+	}
+}
---
+++ Assets/Scripts/ArtManager.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ArtManager : MonoBehaviour {
+
+	public GvrViewer gvrViewer;
+	public GameObject art;
+	public float shiftArtTime = 15f;
+	public Texture[] artMainTextures;
+	public Texture[] artNRMTextures;
+
+	public Color fadeColor = Color.black;
+	public float fadeTime = 5f;
+	public float fadeDelayTime = 1f;
+
+	Material artMaterial;
+	int textureIndex;
+	int textureCount;
+	bool toPause = false;
+	AdjustSizeFromTexture adjustScaleScript;
+
+	IEnumerator coroutine;
+
+	bool toZoomIn = true;
+
+	//Action toScale;
+	Action change_M_T;
+
+	// Use this for initialization
+	void Start () {
+		textureIndex = 0;
+		textureCount = artMainTextures.Length;
+
+		artMaterial = art.GetComponent<Renderer> ().material;
+		adjustScaleScript = art.GetComponent<AdjustSizeFromTexture> ();
+
+		ChangeMatText (artMaterial,
+			artMainTextures[textureIndex%textureCount],
+			artNRMTextures[textureIndex%textureCount]);
+		//
+		coroutine = ShiftArtTexture (shiftArtTime);
+		StartCoroutine (coroutine);
+
+		//toScale += ChangeArtScale;
+		change_M_T += ChangeMaterialAndTexture;
+	}
+
+	public IEnumerator ShiftArtTexture(float waitTime) {
+		do {
+			yield return new WaitForSeconds (waitTime);
+			//
+//			ChangeMatText (artMaterial,
+//				artMainTextures [textureIndex % textureCount],
+//				artNRMTextures [textureIndex % textureCount]);
+			//
+			CameraFade.StartAlphaFadeInOut(fadeColor, fadeTime, fadeDelayTime, change_M_T);
+			//
+			textureIndex++;
+			print ("ShiftArtTexture at " + Time.time);
+
+			// time to change the scale!
+			//adjustScaleScript.AdjustSize();	// move to be inside of ChangeMaterialAndTexture
+
+		} while (!toPause);
+	}
+
+	void Update () {
+		if(Input.GetKeyDown("space")){
+//			StopCoroutine (coroutine);
+			Debug.Log ("pause at " + Time.time);
+			toPause = !toPause;
+		}
+
+		if(gvrViewer.Triggered){
+			Debug.Log ("triggered! Zoom in/out art");
+			ChangeArtScale ();
+			//
+//			CameraFade.StartAlphaFadeInOut(fadeColor, fadeTime, fadeDelayTime, toScale);
+		}
+	}
+
+	void ChangeMatText(Material _mat, Texture _mainTex, Texture _bumpMap){
+		_mat.SetTexture ("_MainTex", _mainTex);
+		_mat.SetTexture ("_BumpMap", _bumpMap);
+	}
+
+	void ChangeMaterialAndTexture(){
+		artMaterial.SetTexture ("_MainTex", artMainTextures [textureIndex % textureCount]);
+		artMaterial.SetTexture ("_BumpMap", artNRMTextures [textureIndex % textureCount]);
+		adjustScaleScript.AdjustSize();
+	}
+
+	void ChangeArtScale(){
+		Vector3 newScale;
+
+		if (toZoomIn)
+			newScale = art.transform.localScale * 3.0F;
+		else
+			newScale = art.transform.localScale / 3.0F;
+
+		art.transform.localScale = newScale;
+
+		toZoomIn = !toZoomIn;
+	}
+}
