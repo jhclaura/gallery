29827451954c98ad0e5554b78f57240c37a5c5f5
jhclaura <jhclaura@gmail.com>
2016-08-24 15:01:33 -0400

    update from master

--- Assets/Scripts/WebManager.cs
+++ Assets/Scripts/WebManager.cs
@@ -7,11 +7,12 @@ public class WebManager : MonoBehaviour {
 	Color[] myNormalMapColor;
 
 	void Start () {
-		string url = "https://lh3.googleusercontent.com/Kldo4P2YHsJDiEZ5w6StZA-flQwD61p_S_o64JkLE8ajqe-tJlfr_jOwZ1DD5QpmHUrHBNUfmGfs7v92d4le7HuPZoFmdz-Wn8y3NDwZQoJBpK4SIH7xqixpCsBc6tg7_c5HKt6nY5GqxKX7vFmo31BwSdZlQ80eIw9IfdzxDAbWz9NOiLQkQBICKRlF9LbLnsl2LIBHk7JOZJu9H566G8jvya40dtKb6JAFUhpbBFdVlmjIphFAX4pZL9aBUSJNP7SWy6mCVAmoTOpzYaRqpK6M46QntNOQu48G3QxEYDOeT_6o1Jf8B8C9WyzMCnY0mkBHpTYur0mg67PKnAOjyK_xLdkDUyazjMQoS3z32cV-1zf4ir7L1ZVyzePVIZsA0PC2VM9ykUT3sm1A2ybpUpT7m5tLaPfPi3DoUMF5AF-wlMn0iShr5MOr9Jt06Tla8fwgGTzgs4Ni0wykpiRXIPvV8U-E3PRGRfFXqcFnib_3_bQmu4ThDQ2GfXT28ROlDvpWJECzTqTcSkET6h_kbrfL6JDeLIQYf6-QGW-AYZNQ5WddT3N0UR_lPdeHeZ2KCYihdPQ1yk8ZX5hvUhFHlf6jOox3TcetWNX2IOa1Cj9xrw_QHQ=w992-h1486-no";
+		string url = "https://lh3.googleusercontent.com/pcMrs1Q4o7dTNzyYd7fx9gb8R_GEM_S_bjrthUVSPHQg-SE1i7JT74BpHiqqEbKW05gx0WgYYebodVZ-MM-POYHi726dgHzjd6fpXZtGDgmGdDWXKzxyCDs6xjJDXrNhiRwhkgg5eW_ndowPishSoLLghHd5vKo0JZhBeVhiXzAZx1St7Mzdem8kEzoUqtCEP_wYuVT-BJiS89288_NR_MUDtCL8nyNtrnvXFJqQzm1B9zfIarxuowuMxDAQF7maqItDFCqDCC9V9Z0hnyfdTNzQnOwJ7xlNyzAF-Y5mwYVwj1aTu92vJRRT9LwGtatr6RGU5Ml7G6Dyqz81_b20atWSFOc1Vw3kYzIELxPoTidq6zDi4IBt2YpqVnWEq0By41EUvPNWhgYAqS_V8LM2dMATtjk_6Pawl6NWuSpbS9_JlAK6i-_nWj7V57Tmpp8HI186PcrAbJ0cxPBNqqvXlUtgCtNkrnf0ts0vzjRN9Wwd0FXNJu4kNL045jg_zex8ZI3jky7UM8tur6JSr9IvBnGIb4j-rnwgVTXZuogxmIIP9SiX7j0aeyjfBuoipfhM3FSXRpQAj_WXx6S_g_2bvq1cuTxheMxwDWhH3zgIbCOoaY_Mng=w1206-h1398-no";
 		WWW www = new WWW(url);
 		StartCoroutine(WaitForRequest(www, "_MainTex"));
 
-		url = "https://lh3.googleusercontent.com/HXECsLoq-mvlUdrJh4aMmAIUz6l_UOlhXM8Tt4pArQCWjrYFmONGVBeeUbbQ6wPMXqmaXSaYZ0QbSG2_Z5VMfKsgphQd6BfEeFe-bolUPaoIEQjV__Xxw-EmHSRYZPbPjdJObDwvmeZq_sw7rbWHCP0x28ohR4WP0E4_yJC52eeIHIczLJ-yhPjJehpOejroeFp2sU35H0k1nTjGH-UBzUWmlwgydh48EhssltGb7dVVztM5o7EfCFzKkMnb69r6_1rPb4lHt0CA-gtzWmly7SaW3gW1ybQr0p6VqXsfezROJrp8ELzSJevhkh_fD2SjCdm0lHTD3j8y1mamc6RE7MH2R9uvHXm5i-nvevuraKWMSPflfe2Pg-KknJHIZk36XpePseFSTinq2Tsp13_YKPxhMll09ei_8PQdcS2gQTTtZigu5d4KVtsXj6H2eTkM_19we8vECv5hgBtr-c8w0mXrhbAlSkGh_aLcbV1z_sqNqSUZ_SIT6GTJlheCCyaLy3uC3drDBzk6vySAhff1E-LkQaY09EA4pqVMwKxqDHTmlnkJreVxYktO46gsQh4g4aV7T2RR0YtVMPUsh5InQynZJUxHbodoo7AcKy3VpdPFhk4hdw=w992-h1486-no";
+		url = "https://lh3.googleusercontent.com/cIS3JE-rXKRuEI7wKbKorvleUtQVHIUgN-o7ivLTIs5FWet3epM3grko1mWeYeMalE3z6hiYuKLjZnSRTlzlax4Rz2xwJSSZXrUwer2mP_VeMn3JRjos1ya7cK9ElspPbeiP4MrbW3nU3fTGCb3ampYLDJsL1e5XdnqK1v1GL6KYBuTB1duhixokCtkke_I0Pb_SXYMOqL0WtSQXKv1fWexeGZbJvy0WqJigyKST5uGY9H0UQf32bwbF9rLbx2N7ixkMMe70e36LjpCOG6-zMfp0D8knbXEWgyF6cJlafRr1M3CQEOGawk6A3PrNKrW-sx9inl_7aybwdruMagXbwyrjrLpRguhmeE0OlshAgax_NOskNhOkt33EvJiR33hNU5rZ15NSNtHVR_Lah2GkFSo8IXg5EQkOC0JItYOs6fDoToy9K60J-cDMX1NUT2cNu87xfcKqQJ9elhUnOxYq0QUzcjuxSMDSDNqaMrs8kUz-xoM6hUYvVx6W_5ypsatw-f-_yK2PI8rr-gvBlnyj34SRU9jkaqJicJVjg2HSWGzQWN4fSlSVqUUMUKVwz_YhEFWMMSej9r0h8U-IJGu9oEr32ea2SyPuGSJGHC6HaNv0E4WSVA=w1206-h1398-no";
+
 		WWW www2 = new WWW (url);
 		StartCoroutine(WaitForRequest(www2, "_BumpMap"));
 	}
@@ -30,7 +31,7 @@ public class WebManager : MonoBehaviour {
 			} else {
 				Texture2D normalMap = new Texture2D (www.texture.width, www.texture.height, TextureFormat.ARGB32, false);
 				myNormalMapColor = www.texture.GetPixels ();
-				for(int i=0; i<myNormalMapColor.Length; i++){
+				for(int i=0; i<myNormalMapColor.Length; i++){					
 					myNormalMapColor [i].a = myNormalMapColor [i].r;
 					myNormalMapColor [i].r = 0;
 					myNormalMapColor [i].b = 0;
