9c06b1d1b8650492534b3dc77999bc2efe72f1c9
jhclaura <linkinmonkey@gmail.com>
2016-09-28 16:56:57 -0500

    add finger collision event

---
+++ Assets/Scripts/HandInteraction.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandInteraction : MonoBehaviour {
+
+    Collider handCollider;
+
+    void Start()
+    {
+        handCollider = GetComponent<Collider>();
+    }
+	void OnCollisionEnter(Collision collision)
+    {
+        //if(collision.gameObject == logo)
+        //{
+            Debug.Log("start touching logo!");
+        //}
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        //if (collision.gameObject == logo)
+        //{
+            Debug.Log("stop touching logo!");
+        //}
+    }
+}
